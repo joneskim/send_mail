99ef459b2b50bcbf1d1627c4cbc49b7a97e5cd6a
Joneskim <joneskimaminiel@gmail.com>
2021-11-20 22:48:11 -0600

    Thirteenth commit

--- codeproject/codeproject/settings.py
+++ codeproject/codeproject/settings.py
@@ -121,7 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Default primary key field type
